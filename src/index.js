780e5a4c5018c6fa80cc868ac2c68d96c79fff64
Mostafa Salama <mostafatourad789@gmail.com>
2020-08-25 10:34:12 +0200

    add user routes to the main app file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const userRoutes = require('./')
+const userRoutes = require('./user/routes');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
 	res.send('Hello world');
 });
-
+app.use('/users', userRoutes);
 app.listen(process.env.PORT || 3000, () => {
 	console.log('app is running at process 3000');
 });
